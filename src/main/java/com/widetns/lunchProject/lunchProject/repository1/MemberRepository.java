b9f10776fd92ea1059d017735ebf8e6a71844536
wndgks49 <89828897+wndgks49@users.noreply.github.com>
2022-06-24 16:10:38 +0900

    멤머 회원 엔티티 및 레파지토리 추가

---
+++ src/main/java/com/widetns/lunchProject/lunchProject/repository1/MemberRepository.java
@@ -0,0 +1,7 @@
+package com.widetns.lunchProject.lunchProject.repository1;
+
+import com.widetns.lunchProject.lunchProject.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, Integer> {
+}
